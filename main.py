6ac218bb94ad6f30998a4846dde2694d1d25256c
Adrián Páez <adriansauce98@gmail.com>
2024-05-14 15:26:42 -0400

    Cambio de diseño

--- main.py
+++ main.py
@@ -1,55 +1,73 @@
 import flet as ft
+# from windows.login import contenedor
+
+contenedor = ft.Container(
+    ft.Column([
+        ft.Container(
+            ft.Text(
+                "Iniciar Sesión",
+                width=320,
+                size=25,
+                weight="w900",
+                font_family="Comfortaa",
+                text_align="center"),
+            padding=ft.padding.only(20, 20)
+        ),
+        ft.Container(
+            ft.TextField(
+                width=280,
+                label="Usuario",
+                prefix_icon=ft.icons.PERSON,
+                border="underline",
+                hint_text="Coloca tu nombre de usuario",
+            ),
+            padding=ft.padding.only(20, 20)
+        ),
+        ft.Container(
+            ft.TextField(
+                width=280,
+                label="Contraseña",
+                prefix_icon=ft.icons.LOCK,
+                border="underline",
+                password=True,
+                can_reveal_password=True
+            ),
+            padding=ft.padding.only(20, 20)
+        ),
+        ft.Container(
+            ft.Checkbox(
+                label="Recordar contraseña"
+            ),
+            padding=ft.padding.only(80)
+        ),
+        ft.Container(
+            ft.ElevatedButton(
+                text= "Entrar",
+                width= 120,
+            ),
+            padding=ft.padding.only(left=100, right=100)
+        )
+    ],
+        ft.MainAxisAlignment.SPACE_EVENLY),
+
+
+
+    width=320,
+    height=400,
+    alignment=ft.alignment.center,
+    border_radius=20,
+    gradient=ft.LinearGradient([
+        "#3E97D3"
+    ]),
+
+)
 
 
 def main(page: ft.Page):
-    contenedor2 = ft.Container(
-        bgcolor='#1ED5B9',
-        border_radius=ft.border_radius.all(20),
-        content=ft.Column(
-            width=300,
-            height=500,
-            controls=[
-                ft.Image()
-            ]
-        )
-    )
-    contenedor = ft.Container(
-        bgcolor="#324359",
-        border_radius=ft.border_radius.all(20),
-        content=ft.Row(
-            width=750,
-            controls=[
-                ft.Column(
-                    width=300,
-                    height=500,
-                    controls=[
-                        ft.Image(
-                            'paper.jpg', fit=ft.ImageFit.COVER, height=1000)
-                    ]
-                ),
-                ft.Column(
-                    spacing=50,
-                    width=300,
-                    height=500,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    controls=[
-                        ft.Text("Iniciar Sesión", color="white",font_family="Comfortaa"),
-                        ft.TextField(icon=ft.icons.EMOJI_EMOTIONS_ROUNDED, label="Nombre",
-                                     hint_text="Coloca tu nombre", width=200, border=ft.InputBorder.UNDERLINE, adaptive=True),
-                        ft.TextField(color='#2BD9A8', icon=ft.icons.LOCK_ROUNDED, label="Contraseña", password=True,
-                                     can_reveal_password=True, width=200, border=ft.InputBorder.UNDERLINE, adaptive=True),
-                        ft.ElevatedButton("Entrar", bgcolor='#1ED5B9')
-                    ]
-                )])
-    )
-
-    page.horizontal_alignment = 'center'
-    page.vertical_alignment = 'center'
-    page.theme = ft.Theme(color_scheme_seed="Indigo")
-    page.title = "Sistema Administrativo"
+    page.bgcolor = "black"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
     page.add(contenedor)
 
-    # page.theme_mode = ft.ThemeMode.SYSTEM
 
-
-ft.app(main)
+ft.app(target=main)
---
+++ windows/login.py
@@ -0,0 +1,14 @@
+# Ventana de login
+import flet as ft
+
+
+contenedor = ft.Container(
+    width= 390,
+    height= 500,
+    border_radius= 20,
+    gradient= ft.LinearGradient([
+        ft.colors.AMBER_ACCENT,
+        ft.colors.TEAL
+    ])
+    
+)
